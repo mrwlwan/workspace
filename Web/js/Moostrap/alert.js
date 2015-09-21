e003fae6370f8a0c416455648b0819c2138bb35d
mrwlwan <mrwlwan@gmail.com>
2015-09-22 00:25:20 +0800

    Update Moostrap/alert.js

--- Web/js/Moostrap/alert.js
+++ Web/js/Moostrap/alert.js
@@ -1,32 +1,28 @@
 Moostrap.Alert = new Class({
-    Implements: [Options, Events],
+    Extends: Moostrap.Component,
     options: {
+        store_name: 'alert',
+        selector: '[data-dismiss=alert]',
         duration: 150
     },
 
-    initialize: function(closer){
-        this.closer= closer
+    initialize: function(element){
+        this.parent(element)
         this.container = this.get_container()
     },
 
-    toElement: function(){
-        return this.container
-    },
-
     get_container: function(){
-        return Moostrap.get_target(this.closer) || this.closer.getParent('.alert')
+        return this.get_target(this.element) || this.element.getParent('.alert')
     },
 
     close: function(){
         this.fireEvent('close')
         this.container.removeClass('in')
         if(Moostrap.transition && this.container.hasClass('fade')){
-            //this.container.addEventListener(Moostrap.transition, function(){
-                (function(){
-                    this.container.destroy()
-                    this.fireEvent('closed')
-                }).delay(this.options.duration, this)
-            //}.bind(this))
+            (function(){
+                this.container.destroy()
+                this.fireEvent('closed')
+            }).delay(this.options.duration, this)
         }else{
             this.container.destroy()
             this.fireEvent('closed')
@@ -38,11 +34,7 @@ Moostrap.Alert = new Class({
 window.addEvent('domready', function(){
     $(document.body).addEvent('click:relay([data-dismiss=alert])', function(e, target){
         e.preventDefault()
-        var alert_window = target.retrieve('alert')
-        if(!alert_window){
-            alert_window = new Moostrap.Alert(target)
-            target.store('alert', alert_window)
-        }
+        var alert_window = target.retrieve('alert') || new Moostrap.Alert(target)
         alert_window.close()
     })
 })
