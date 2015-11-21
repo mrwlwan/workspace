6bb7fa158a7263e6ccc62a807961ebe36803679e
mrwlwan <mrwlwan@gmail.com>
2015-11-21 16:23:40 +0800

    Update kisrequest.

--- Python/libs/kisrequest.py
+++ Python/libs/kisrequest.py
@@ -1,10 +1,10 @@
 # coding=utf8
 
-from http.cookiejar import CookieJar, LWPCookieJar
+import http.cookiejar
 import urllib.request, urllib.parse, re, json, os
 
 class Session:
-    def __init__(self, cookiejar=None, headers=[], handlers=[]):
+    def __init__(self, cookiejar=None, headers=None, handlers=None):
         """ 初始化 Session 对象.
         @cookiejar(http.cookiejar.CookieJar/str): str 时自动转换成 http.cookiejar.LWPCookieJar 对象.
         @headers(2-tuples): 格式[(key, value), ...].
@@ -12,16 +12,16 @@ class Session:
         """
         self.opener = urllib.request.build_opener()
         # 默认添加 HTTPCookieProcessor
-        if cookiejar and isinstance(cookiejar, CookieJar):
+        if cookiejar and isinstance(cookiejar, http.cookiejar.CookieJar):
             self.cookiejar = cookiejar
         else:
-            self.cookiejar = LWPCookieJar(cookiejar)
+            self.cookiejar = http.cookiejar.LWPCookieJar(cookiejar)
             if cookiejar and os.path.exists(cookiejar): self.cookiejar.load()
-        handlers.append(urllib.request.HTTPCookieProcessor(self.cookiejar))
-        for handler in handlers:
-            self.opener.add_handler(handler)
+        self.opener.add_handler(urllib.request.HTTPCookieProcessor(self.cookiejar))
+        if handlers:
+            for handler in handlers: self.opener.add_handler(handler)
         # 默认添加 Firefox useragent.
-        self.opener.addheaders = headers or [('User-agent', 'Mozilla/5.0')]
+        self.opener.addheaders = headers or [('User-agent', 'Mozilla/5.0 (Windows NT 5.1; rv:42.0) Gecko/20100101 Firefox/42.0')]
 
     def add_header(self, header):
         self.opener.addheaders.append(header)
@@ -29,7 +29,7 @@ class Session:
     def add_handler(self, handler):
         self.opener.add_handler(handler)
 
-    def request(self, url, params={}, data=None, method=None, headers=[], proxies=[], timeout=None, encoding='utf8', errors='strict', doseq=False, safe='', origin_req_host=None, unverifiable=False, **kwargs):
+    def request(self, url, data=None, params={}, method=None, headers=[], proxies=[], timeout=None, encoding='utf8', errors='strict', doseq=False, safe='', origin_req_host=None, unverifiable=False, **kwargs):
         """ 通用 http 请求, 返回 ResponseProxy 对象.
         @params(dict/2-tuples): url queries.
         @data(str/dict/2-tuples).
@@ -54,7 +54,7 @@ class Session:
         for proxy in proxies:
             r.set_proxy(*proxy)
         response = self.opener.open(r, timeout=timeout, **kwargs)
-        return ResponseProxy(response, r)
+        return ResponseProxy(response, None)
 
     def get(self, url, **kwargs):
         return self.request(url, method='GET', **kwargs)
@@ -73,7 +73,7 @@ class Session:
 
 
 class ResponseProxy:
-    def __init__(self, response, request):
+    def __init__(self, response, request=None):
         """ 保留关键属性: _encoding, _content """
         self.response = response
         self.request = request
@@ -89,7 +89,7 @@ class ResponseProxy:
     @property
     def encoding(self):
         if not hasattr(self, '_encoding'):
-            search = re.search(r'charset=\s*([a-z0-9_\-]+)', self.content_type, re.I)
+            search = re.search(r'charset=\s*([a-z0-9_\-]+)', self.content_type or '', re.I)
             self._encoding = search and search.group(1) or None
         return self._encoding
 
@@ -102,8 +102,8 @@ class ResponseProxy:
     def get_text(self, encoding=None, errors='strict'):
         return self.content.decode(encoding or self.encoding or 'utf8', errors)
 
-    def get_json(self, default=None, encoding=None, errors='strict'):
-        text = self.get_text(encoding, errors).strip()
+    def get_json(self, default=None, strip=None, encoding=None, errors='strict'):
+        text = self.get_text(encoding, errors).strip(strip)
         return json.loads(text) if text else default
 
     def get_header(self, key, default=None):
